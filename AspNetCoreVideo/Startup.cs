906ca39795ae3b7af251955abd15ce2ceb4f38d2
Sean Drohan <sean.drohan@gmail.com>
2019-02-04 16:24:51 -0500

    Changed Hello Message

--- AspNetCoreVideo/Startup.cs
+++ AspNetCoreVideo/Startup.cs
@@ -27,7 +27,7 @@ namespace AspNetCoreVideo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello from my World! Hello again.");
             });
         }
     }
